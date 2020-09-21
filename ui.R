6b38bd05ffc3545b53f4a6f569b0364e8efba4e0
petepetersen423 <petepetersen423@gmail.com>
2020-09-20 22:40:21 -0700

    after deploying

--- ui.R
+++ ui.R
@@ -5,7 +5,8 @@ library(shiny)
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("MTCARS Feature Exploration"),
+    titlePanel("MTCARS Feature Exploration",
+               ),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
@@ -15,15 +16,22 @@ shinyUI(fluidPage(
                         min = 5,
                         max = 10,
                         value = 6),
+            h5("1.  Move the slider to select the number of bins for the 3 histograms 
+               displaying features of the MTCARS dataset", align = "left"),
             selectInput("color", "Choose a color:",
-                        choices = c("turquoise", "aquamarine", "darkgreen","steelblue"))
+                        choices = c("turquoise", "aquamarine", "darkgreen","steelblue")),
             
            
+           
+            h5("2.  Select a color from the dropdown list to apply to the 3 histograms 
+               displaying features of the MTCARS dataset.", align = "left"),
             
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
+            h5("By:  Pete Petersen"),
+            h5("Course:  Data Products"),
             plotOutput("distPlot"),
             plotOutput("wgtplot"),
             plotOutput("carbplot")
